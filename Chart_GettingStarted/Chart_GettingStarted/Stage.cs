b122714561d69676652899cbc3cab74e7147eed8
SaiyathAliFathima <103025761+SaiyathAliFathima@users.noreply.github.com>
2022-12-20 16:52:37 +0530

    master

---
+++ Chart_GettingStarted/Chart_GettingStarted/Stage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chart_GettingStarted
+{
+    public class Stage
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
