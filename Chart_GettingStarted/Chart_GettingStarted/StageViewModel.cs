729828a3b49752824d4ecbed3ed94d7079043bb7
EzhilarasanElangovan31 <ezhilarasan.elangovan@syncfusion.com>
2024-10-30 14:33:52 +0530

    commit

---
+++ Chart_GettingStarted/Chart_GettingStarted/StageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chart_GettingStarted
+{
+    public class StageViewModel
+    {
+        public List<StageModel> Data { get; set; }
+
+        public StageViewModel()
+        {
+            Data = new List<StageModel>()
+            {
+                new StageModel(){Name = "Stage A", Value=12},
+                new StageModel(){Name = "Stage B", Value=21},
+                new StageModel(){Name = "Stage C", Value=29},
+                new StageModel(){Name = "Stage D", Value=37},
+            };
+        }
+    }
+}
